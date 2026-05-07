7f7acfb49208b970cd21af084a33d348b94b5ba2
HemPradeepthummala <pradeeproyalthummala@Thummalas-MacBook-Pro.local>
2026-05-07 11:12:27 +0530

    setuo directory structure

---
+++ src/com/tw/bootcamp/problem1/Sample.java
@@ -0,0 +1,7 @@
+package com.tw.bootcamp.problem1;
+
+public class Sample {
+	static void main() {
+		System.out.println(2+7);
+	}
+}
---
+++ test/com/tw/bootcamp/problem1/SampleTest.java
@@ -0,0 +1,9 @@
+package com.tw.bootcamp.problem1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+ void sampleTest () {
+
+ }
+}
